a289471ef49d8f434865013181e10e974b2a4f3a
Phu Huynh <phu@filesoft.co>
2021-11-18 15:07:32 +0700

    Initial Commit

---
+++ MyTeam/MyTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTeamApp.swift
+//  MyTeam
+//
+//  Created by Huong on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
